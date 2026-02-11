d48c087cb9342b1195a39ec66b2658a34b411c06
amq-tutti <tutti-amq@proton.me>
2026-02-12 01:13:31 +0200

    Update title in config.js to 'ReoNa Party Rank Sorter'

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
     localStoragePrefix: "reona-pr",
-    title: "Party Rank Sorter",
+    title: "ReoNa Party Rank Sorter",
     description: "Party rank sorter for your custom list of songs."
 };
